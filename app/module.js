dd2becfbcef11ee56a2759fa8024ae06fa5f4e67
Abdus Salam <salam@Aptechnk1.edu>
2021-01-05 13:33:27 +0500

    add

--- app/module.js
+++ app/module.js
@@ -5,47 +5,42 @@ myninja.controller('ninjacontroller',['$scope', function($scope){
 
     $scope.removeninja = function(ninja){
        
-        var removeninja = $scope.ninjas.indexOf(ninja);
-        $scope.ninjas.splice(removeninja, 1);
+        var removeninja = $scope.fil.indexOf(ninja);
+        $scope.fil.splice(removeninja, 1);
     };
 
     $scope.addninja = function(){
-        $scope.ninjas.push({
-            name: $scope.newninja.name,
-            rate: parseInt($scope.newninja.rate),
-            belt: $scope.newninja.belt,
-            available: true,
+        $scope.fil.push({
+            product: $scope.newninja.product,
+            price: $scope.newninja.price,
+            color: $scope.newninja.color,
         });
 
-        $scope.newninja.name= "";
-        $scope.newninja.age = "";
-        $scope.newninja.exp = "";
+        $scope.newninja.product= "";
+        $scope.newninja.price = "";
+        $scope.newninja.color = "";
     }
  
-    $scope.ninjas = [
+    $scope.fil = [
         {
             product: "Shirt",
             price: "34",
             color: "yellow",
-            available:true,
         },
         {
             product: "Shoes",
             price: "54",
             color: "red",
-            available:true,
         },
         {
             product: "Pent",
             price: "44",
             color: "brown",
-            available:true,
         },
         {
             product: "ali",
             price: "54",
             color: "yellow",
-            available:true,
         },
       ]
 
